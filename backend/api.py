7432a103467e150a7d18a0f54995dfd47d51e490
Rohan Awhad <rohanavad007@gmail.com>
2023-02-19 14:29:42 +0530

    fixed bug where earlier I was passing .text from readable response to BS4. Now resolved it to pass correct input

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,16 @@
+import requests
+
+from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from bs4 import BeautifulSoup
-import requests
+from loguru import logger
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=['OPTIONS', 'GET'],
+    allow_methods=['*'],
     allow_headers=['*'],
 )
 READABLE_API = 'http://localhost:50501/convert'
@@ -20,14 +22,23 @@ def summarize(url: str):
     # Use beautiful soup lib to get the text from article_contanet
     # send the text to summarizer engine and get the summary
     # return the summary
+    logger.info(f'Summarizing : {url}')
+    logger.info('Calling readability API')
     content = requests.get(READABLE_API, params={'url': url})
     if content.status_code != 200:
         raise HTTPException(status_code=content.status_code, detail=content.text)
-    soup = BeautifulSoup(content.text, 'lxml')
-    text = soup.get_text()
+    logger.info('Readability API call successful')
+    article_content = content.json()['article_content']
+    logger.debug(f'\nArticle content : {article_content}')
+    soup = BeautifulSoup(article_content, 'lxml')
+    text = soup.text
+    logger.debug(f'\nArticle text : {text}')
+    logger.info('Calling summarizer engine')
     summary = requests.get(SUMMARIZER_API, params={'text': text})
     if summary.status_code != 200:
         raise HTTPException(status_code=summary.status_code, detail=summary.text)
+    logger.info('Summarizer engine call successful')
+    logger.debug(f'\nSummary : {summary.text}')
     return {'summary': summary.text}
 
 
