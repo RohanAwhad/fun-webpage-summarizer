4db269939c7cee1925898d07fd13126901c80ddc
Rohan Awhad <rohanavad007@gmail.com>
2023-03-10 10:01:29 +0530

    testing streaming code

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,14 @@
 import requests
+import time
 
 from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 from loguru import logger
+
+import summarizer
+from readability import Readable
 
 app = FastAPI()
 app.add_middleware(
@@ -13,35 +18,44 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
-READABLE_API = 'http://localhost:50501/convert'
-SUMMARIZER_API = 'http://localhost:50507/summarize'
 
 @app.get('/summarize')
 def summarize(url: str):
-    # Call the readable API and get article_content
-    # Use beautiful soup lib to get the text from article_contanet
-    # send the text to summarizer engine and get the summary
-    # return the summary
     logger.info(f'Summarizing : {url}')
+
     logger.info('Calling readability API')
-    content = requests.get(READABLE_API, params={'url': url})
-    if content.status_code != 200:
-        raise HTTPException(status_code=content.status_code, detail=content.text)
+    content = Readable(url)
     logger.info('Readability API call successful')
-    article_content = content.json()['article_content']
+
+    article_content = content.article_content
     logger.debug(f'\nArticle content : {article_content}')
     soup = BeautifulSoup(article_content, 'lxml')
     text = soup.text
     logger.debug(f'\nArticle text : {text}')
+
     logger.info('Calling summarizer engine')
+    summary = summarizer.summarize(text)
+    '''
     summary = requests.get(SUMMARIZER_API, params={'text': text})
     if summary.status_code != 200:
         raise HTTPException(status_code=summary.status_code, detail=summary.text)
+    '''
     logger.info('Summarizer engine call successful')
-    logger.debug(f'\nSummary : {summary.text}')
-    return {'summary': summary.text}
+    logger.debug(f'\nSummary : {summary}')
+    return {'summary': summary}
 
+
+@app.get('/stream')
+def _stream():
+    def iter_text():
+        text = '''A static library (also known as an archive) consists of routines that are compiled and linked directly into your program. When you compile a program that uses a static library, all the functionality of the static library that your program uses becomes part of your executable. On Windows, static libraries typically have a .lib extension, whereas on Linux, static libraries typically have an .a (archive) extension. One advantage of static libraries is that you only have to distribute the executable in order for users to run your program. Because the library becomes part of your program, this ensures that the right version of the library is always used with your program. Also, because static libraries become part of your program, you can use them just like functionality you’ve written for your own program. On the downside, because a copy of the library becomes part of every executable that uses it, this can cause a lot of wasted space. Static libraries also can not be upgraded easy -- to update the library, the entire executable needs to be replaced.'''
+
+        for word in text.split():
+            time.sleep(0.5)
+            yield word
+
+    return StreamingResponse(iter_text(), media_type='text/html')
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=50502)
+    uvicorn.run(app, host='0.0.0.0', port=50501)
--- extension/script.js
+++ extension/script.js
@@ -2,12 +2,41 @@ async function getReadableVersion(url) {
     if (url === undefined) {
         return "No URL found"
     }
-    const res = await fetch ('http://localhost:50502/summarize?url='+url)
+    const res = await fetch ('http://localhost:50501/summarize?url='+url)
     const record = await res.json()
     return record['summary']
 }
 
+async function* streamAsyncIterator(stream) {
+    const reader = stream.getReader();
+
+    try {
+        while (true) {
+            const {done, value} = await reader.read();
+            if (done) return;
+            let word = '';
+            await value.forEach( x => { word += String.fromCharCode(parseInt(x)) });
+            word += ' ';
+            console.log(value);
+            console.log(word);
+            yield word
+            //yield String.fromCharCode(value);
+        }
+    } finally {
+        reader.releaseLock();
+    }
+}
+
+async function getStream(url) {
+    const res = await fetch ('http://localhost:50501/stream')
+    for await (const chunk of streamAsyncIterator(res.body)) {
+        document.getElementById('summary').innerHTML += chunk;
+        //console.log(chunk)
+    }
+}
+
 
 chrome.tabs.query({currentWindow: true, active: true}, async function(tabs) {
-    document.getElementById('summary').innerHTML = await getReadableVersion(tabs[0].url)
+    await getStream(tabs[0].url)
+    //document.getElementById('summary').innerHTML = await getReadableVersion(tabs[0].url)
 });
